8f122603c9c94f3d5828e4a74e302af571b00c74
mohamk29 <mohamk29@my.yorku.ca>
2023-08-24 18:23:54 -0400

    Added the form as an overlay and implemented functionality to remove the overlay upon clicking the backdrop

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,10 +2,16 @@ import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
+import Modal from "./Modal";
 
 const PostList = (props) => {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  const hideModalHandler = () => {
+    setModalIsVisible(false);
+  };
 
   const bodyChangeHandler = (event) => {
     setEnteredBody(event.target.value);
@@ -14,12 +20,17 @@ const PostList = (props) => {
   const authorChangeHandler = (event) => {
     setEnteredAuthor(event.target.value);
   };
+
   return (
     <>
-      <NewPost
-        onBodyChange={bodyChangeHandler}
-        onAuthorChange={authorChangeHandler}
-      />
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Manuel" body="Check out the full course!" />
