0671aad70d0e4ff7969f01dbf4510d5596aa6c14
mohamk29 <mohamk29@my.yorku.ca>
2023-08-24 17:59:53 -0400

    Added a form component(NewPost) with styling, connected event listeners on the appropriate fields and lifted state up to connect with PostList component.

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 
 const PostList = (props) => {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  const bodyChangeHandler = (event) => {
+    setEnteredBody(event.target.value);
+  };
+
+  const authorChangeHandler = (event) => {
+    setEnteredAuthor(event.target.value);
+  };
   return (
-    <ul className={classes.posts}>
-      <Post author="Maximilan" body="React.js is awsome" />
-      <Post author="Manuel" body="Check out the full course!" />
-    </ul>
+    <>
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Manuel" body="Check out the full course!" />
+      </ul>
+    </>
   );
 };
 
