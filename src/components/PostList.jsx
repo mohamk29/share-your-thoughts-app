5ef269e6ff0b218bc7c399ca8352d042bda09d46
mohamk29 <mohamk29@my.yorku.ca>
2023-08-25 16:48:39 -0400

    Transfered state management from PostList component to NewPost component to create a list of dynamic posts that the user can create

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -5,30 +5,22 @@ import classes from "./PostList.module.css";
 import Modal from "./Modal";
 
 const PostList = (props) => {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
+  const [posts, setPosts] = useState([]);
 
-  const bodyChangeHandler = (event) => {
-    setEnteredBody(event.target.value);
-  };
-
-  const authorChangeHandler = (event) => {
-    setEnteredAuthor(event.target.value);
+  const addPostHandler = (postData) => {
+    setPosts((prevPosts) => {
+      return [postData, ...prevPosts];
+    });
   };
 
   return (
     <>
       {props.modalIsVisible && (
         <Modal onClose={props.onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={props.onStopPosting}
-          />
+          <NewPost onAddPost={addPostHandler} onCancel={props.onStopPosting} />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Manuel" body="Check out the full course!" />
       </ul>
     </>
