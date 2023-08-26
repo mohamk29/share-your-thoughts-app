7ef5455c6b128b5e110e8e71cdc150b258018638
mohamk29 <mohamk29@my.yorku.ca>
2023-08-26 16:57:21 -0400

    Added a get method to recieve the data from the backend and render it for the user to see

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -6,6 +6,19 @@ import Modal from "./Modal";
 
 const PostList = (props) => {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
+    }
+
+    fetchPosts();
+  }, []);
 
   const addPostHandler = (postData) => {
     setPosts((prevPosts) => {
@@ -27,7 +40,7 @@ const PostList = (props) => {
           <NewPost onAddPost={addPostHandler} onCancel={props.onStopPosting} />
         </Modal>
       )}
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => {
             return (
@@ -36,10 +49,15 @@ const PostList = (props) => {
           })}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading posts...</p>
         </div>
       )}
     </>
