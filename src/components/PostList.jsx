8b174ae48d2454e3652dc0a57bd398a12c5d298f
mohamk29 <mohamk29@my.yorku.ca>
2023-08-26 19:08:44 -0400

    Added routes so that the user can navigate to mainpage vs newpost page with the proper url

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
-import NewPost from "./NewPost";
+
 import Post from "./Post";
 import classes from "./PostList.module.css";
-import Modal from "./Modal";
 
-const PostList = (props) => {
+function PostsList() {
   const [posts, setPosts] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
 
@@ -20,33 +19,24 @@ const PostList = (props) => {
     fetchPosts();
   }, []);
 
-  const addPostHandler = (postData) => {
-    setPosts((prevPosts) => {
-      fetch("http://localhost:8080/posts", {
-        method: "POST",
-        body: JSON.stringify(postData),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      return [postData, ...prevPosts];
+  function addPostHandler(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
-  };
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
 
   return (
     <>
-      {props.modalIsVisible && (
-        <Modal onClose={props.onStopPosting}>
-          <NewPost onAddPost={addPostHandler} onCancel={props.onStopPosting} />
-        </Modal>
-      )}
       {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
-          {posts.map((post) => {
-            return (
-              <Post key={post.body} author={post.author} body={post.body} />
-            );
-          })}
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
         </ul>
       )}
       {!isFetching && posts.length === 0 && (
@@ -62,6 +52,6 @@ const PostList = (props) => {
       )}
     </>
   );
-};
+}
 
-export default PostList;
+export default PostsList;
